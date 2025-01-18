aa55a7224eb3b002521fc155e581638b19da9040
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-01-18 17:50:13 +0530

    App.jsx

--- currencyconvertor/src/App.jsx
+++ currencyconvertor/src/App.jsx
@@ -25,7 +25,7 @@ setconvertedAmount(amount*data[to]);
   return (
     <>
     <div className="flex justify-center items-center h-screen w-full bg-gray-400">
-     <div className="flex justify-center flex-col items-center relative bg-white bg-opacity-30 backdrop-blur-lg p-6 shadow-xl w-2/5  rounded-lg py-8 px-4">
+     <div className="flex justify-center flex-col items-center relative bg-white bg-opacity-30 backdrop-blur-lg p-6 shadow-xl   rounded-lg py-8 px-4">
      <InputBox label="From"  selectCurrency={from} currencyOptions={options} onAmountChange={(amnt)=>setAmount(amnt)} onCurrencyChange={(curr)=>setfrom(curr)} amount={amount} />
      <button className="bg-blue-800 text-white px-6 py-2 rounded-md my-[-10px] relative" onClick={swap}>SWAP</button>
      <InputBox disabled label="To"  selectCurrency={to} amount={convertedamount} onAmountChange="" onCurrencyChange={(curr)=>setTo(curr)} currencyOptions={options}/>
