fb0176e7e18d4edb3c17c70e6f06ebf67312b572
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-02-17 22:26:22 +0530

    new method

--- contextproject2/src/App.jsx
+++ contextproject2/src/App.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import './App.css'
-import Dashboard from './components/Dashboard.jsx'
-import Login from './components/Login.jsx'
-import { UserContextProvider } from './context/UerContextProvider.jsx'
-function App() {
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-  return (
-  
-     <UserContextProvider>
-<Login/>
-<Dashboard/>
-     </UserContextProvider>
-   
-     
-  
-  )
+import Card from "./components/Card.jsx";
+import ThemeBtn from "./components/ThemeBtn.jsx";
+import { ThemeProvider } from "./context/Theme.js";
+function App() {
+const [themeMode,setThememode]=useState("light")
+
+const lightTheme=()=>{
+  setThememode("light")
 }
 
-export default App
+const darkTheme=()=>{
+  setThememode("dark")
+}
+
+useEffect(()=>{
+document.querySelector('html').classList.remove("light","dark");
+document.querySelector("html").classList.add(themeMode)
+},[themeMode])
+
+  return (
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+<ThemeBtn/>
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+
+        <div className="w-full max-w-sm mx-auto">
+          <Card/>
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
